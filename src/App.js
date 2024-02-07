4ea6706b04ffe1feb3c582bec574de272a2a38a4
unknown <konankanjulius10@gmail.com>
2024-02-07 08:49:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { LiaToggleOffSolid, LiaToggleOnSolid } from "react-icons/lia";
+import { Info } from './Data';
+import { useState } from 'react';
 
 function App() {
+  const [activatedStates, setActivatedStates] = useState(Info.map(() => false));
+
+  const toggleActivation = (index) => {
+    const newActivatedStates = [...activatedStates];
+    newActivatedStates[index] = !newActivatedStates[index];
+    setActivatedStates(newActivatedStates);
+    console.log("c'est okey");
+  };
+
+  const infos = Info.map((e, index) => (
+    <div key={index} className={`bg-neutral-300 p-3 flex justify-center item-center w-full ${activatedStates[index] ? 'activated' : ''}`}>
+      <div className='w-8/12 bg-white h-24 rounded-xl  flex justify-around items-center'>
+        <p className='text-black w-6/12 text-3xl'>{e.name}</p>
+        <div className=''>
+          <LiaToggleOnSolid className={`text-7xl transition text-green-500 ${activatedStates[index] ? 'hidden duration-300' : 'duration-300'}`} onClick={() => toggleActivation(index)} />
+          <LiaToggleOffSolid className={`text-7xl text-neutral-400 ${activatedStates[index] ? 'duration-300' : 'hidden duration-300'}`} onClick={() => toggleActivation(index)} />
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-neutral-300 h-[1095px]">
+      {infos}
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,23 @@
+
+export const Info = [
+    {
+        index: 1,
+        name: 'Humilité 😇',
+    },
+    {
+        index: 2,
+        name: 'Clash 👿',
+    },
+    {
+        index: 3,
+        name: 'Paiya 🍾',
+    },
+    {
+        index: 4,
+        name: 'Immaturité 😂',
+    },
+    {
+        index: 5,
+        name: 'Prière 👼',
+    },
+]
